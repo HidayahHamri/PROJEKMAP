9f5d7b3bf4cf9cd5520c26bc8132da2d0d4ec64e
HidayahHamri <nurhidayahhamri@gmail.com>
2022-01-19 23:40:32 +0800

    update

--- lib/Model/Booking.dart
+++ lib/Model/Booking.dart
@@ -6,12 +6,12 @@ class Booking {
 class BookingData {
   final String uid;
   final String selectedBookingdate;
-  final String selectedRoom;
+  final String selectedSportPlace;
   final String selectedTimeSlot;
 
   BookingData(
       {this.uid,
       this.selectedBookingdate,
-      this.selectedRoom,
+      this.selectedSportPlace,
       this.selectedTimeSlot});
 }
---
+++ lib/Model/CollegeCourtBooking.dart
@@ -0,0 +1,8 @@
+class CollegeCourtBooking {
+  final String selectedBookingdate;
+  final String selectedCourt;
+  final String selectedTimeSlot;
+
+  CollegeCourtBooking(
+      {this.selectedBookingdate, this.selectedCourt, this.selectedTimeSlot});
+}
