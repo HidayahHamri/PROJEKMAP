0e2fcf5932695bd09c8b32ab3bbccbc87061fcdd
HidayahHamri <nurhidayahhamri@gmail.com>
2022-01-19 22:45:33 +0800

    update

--- lib/Screens/UserProfile/profile_body.dart
+++ lib/Screens/UserProfile/profile_body.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
 //import 'package:library_booking_system/Services/auth_provider.dart';
-import 'package:provider/provider.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:library_booking_system/Model/Users.dart';
-import 'package:library_booking_system/Services/Database.dart';
 
 class ProfileBody extends StatefulWidget {
   @override
@@ -12,90 +8,70 @@ class ProfileBody extends StatefulWidget {
 }
 
 class _ProfileBodyState extends State<ProfileBody> {
-  String username, fullname, contact;
+  String fullname, email, contact;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: FutureBuilder(
-            future: _fetch(),
-            builder: (context, snapshot) {
-              if (snapshot.connectionState != ConnectionState.done)
-                return Center(
-                  child: CircularProgressIndicator(),
-                );
-              return SafeArea(
-                  child: Column(
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.only(top: 60.0),
-                    child: Center(
-                      child: Container(
-                          width: 500,
-                          height: 150,
-                          child: Image.asset('Images/Profile.png')),
-                    ),
-                  ),
-                  Padding(
-                      padding: const EdgeInsets.only(
-                          left: 60.0, right: 60.0, top: 70, bottom: 0),
-                      child: Center(
-                        child: TextFormField(
-                          enabled: false,
-                          decoration: InputDecoration(
-                              border: OutlineInputBorder(),
-                              labelText: "$fullname",
-                              labelStyle: TextStyle(
-                                  color: Colors.blueGrey, fontSize: 22)),
-                        ),
-                      )),
-                  Padding(
-                      padding: const EdgeInsets.only(
-                          left: 60.0, right: 60.0, top: 30, bottom: 0),
-                      child: Center(
-                        child: TextFormField(
-                          enabled: false,
-                          decoration: InputDecoration(
-                              border: OutlineInputBorder(),
-                              labelText: "$username",
-                              labelStyle: TextStyle(
-                                  color: Colors.blueGrey, fontSize: 22)),
-                        ),
-                      )),
-                  Padding(
-                      padding: const EdgeInsets.only(
-                          left: 60.0, right: 60.0, top: 30, bottom: 0),
-                      child: Center(
-                        child: TextFormField(
-                          enabled: false,
-                          decoration: InputDecoration(
-                              border: OutlineInputBorder(),
-                              labelText: "$contact",
-                              labelStyle: TextStyle(
-                                  color: Colors.blueGrey, fontSize: 22)),
-                        ),
-                      )),
-                ],
-              ));
-            }));
-  }
-
-  _fetch() async {
-    final firebaseUser = await FirebaseAuth.instance.currentUser;
-    if (firebaseUser != null)
-      await FirebaseFirestore.instance
-          .collection('Users')
-          .doc(firebaseUser.uid)
-          .get()
-          .then((ds) {
-        fullname = ds.data()['fullname'];
-        username = ds.data()['username'];
-        contact = ds.data()['contact'];
-        print(fullname);
-        print(username);
-        print(contact);
-      }).catchError((e) {
-        print(e);
-      });
+    return Scaffold(body: FutureBuilder(
+        //future: _fetch(),
+        builder: (context, snapshot) {
+      if (snapshot.connectionState != ConnectionState.done)
+        return Center(
+          child: CircularProgressIndicator(),
+        );
+      return SafeArea(
+          child: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(top: 60.0),
+            child: Center(
+              child: Container(
+                  width: 500,
+                  height: 150,
+                  child: Image.asset('Images/Profile.png')),
+            ),
+          ),
+          Padding(
+              padding: const EdgeInsets.only(
+                  left: 60.0, right: 60.0, top: 70, bottom: 0),
+              child: Center(
+                child: TextFormField(
+                  enabled: false,
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: "Fullname",
+                      labelStyle:
+                          TextStyle(color: Colors.blueGrey, fontSize: 22)),
+                ),
+              )),
+          Padding(
+              padding: const EdgeInsets.only(
+                  left: 60.0, right: 60.0, top: 30, bottom: 0),
+              child: Center(
+                child: TextFormField(
+                  enabled: false,
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: "Email",
+                      labelStyle:
+                          TextStyle(color: Colors.blueGrey, fontSize: 22)),
+                ),
+              )),
+          Padding(
+              padding: const EdgeInsets.only(
+                  left: 60.0, right: 60.0, top: 30, bottom: 0),
+              child: Center(
+                child: TextFormField(
+                  enabled: false,
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: "Contact",
+                      labelStyle:
+                          TextStyle(color: Colors.blueGrey, fontSize: 22)),
+                ),
+              )),
+        ],
+      ));
+    }));
   }
 }
