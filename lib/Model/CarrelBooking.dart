f6112cb0cfc28246ac9641f30b742860e8b1a8b2
HidayahHamri <nurhidayahhamri@gmail.com>
2022-01-19 23:41:41 +0800

    yang ni delete

--- lib/Model/CarrelBooking.dart
+++
@@ -1,8 +0,0 @@
-class CarrelBooking {
-  final String selectedBookingdate;
-  final String selectedRoom;
-  final String selectedTimeSlot;
-
-  CarrelBooking(
-      {this.selectedBookingdate, this.selectedRoom, this.selectedTimeSlot});
-}
